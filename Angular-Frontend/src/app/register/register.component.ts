80ebb2d6b4c0aca62d999e661a5e7f231ab1c9a0
saiprashanth202 <31367376+saiprashanth202@users.noreply.github.com>
2017-09-15 15:09:12 +0530

    commit

--- Angular-Frontend/src/app/register/register.component.ts
+++ Angular-Frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  
 
   ngOnInit() {
   }
